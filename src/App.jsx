8e2e3e313c97f621dbca4e19a306cbf86dd5c253
Avinashpotnuru <potnuruaavinash111@example.com>
2025-05-01 14:46:17 +0530

    fix css bugs

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./app.css";
+import "./App.css";
 import Info from "./components/Info";
 import About from "./components/About";
 import Interest from "./components/Interest";
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,52 +1,48 @@
 import React from "react";
-import { FaFacebook, FaGithub, FaInstagram, FaTwitter, FaWhatsapp } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaInstagram, FaWhatsapp } from "react-icons/fa";
 
 export default function Footer() {
+  const socialLinks = [
+    {
+      href: "https://github.com/Avinashpotnuru",
+      label: "GitHub",
+      icon: <FaGithub color="white" size={24} />,
+    },
+    {
+      href: "https://www.facebook.com/share/18qeB3B9mq/",
+      label: "Facebook",
+      icon: <FaFacebook color="white" size={24} />,
+    },
+    {
+      href: "https://www.instagram.com/avi__potnuru",
+      label: "Instagram",
+      icon: <FaInstagram color="white" size={24} />,
+    },
+    {
+      href: "https://api.whatsapp.com/send/?phone=918919016096",
+      label: "WhatsApp",
+      icon: <FaWhatsapp color="white" size={24} />,
+      className: "text-green-500 text-3xl",
+    },
+  ];
+
   return (
     <>
       <footer className="section_footer">
         <ul className="footer_social flex gap-4">
-          <li>
-            <a
-              href="https://github.com/Avinashpotnuru"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="GitHub"
-            >
-              <FaGithub color="white" size={24} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.facebook.com/share/18qeB3B9mq/"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Twitter"
-            >
-              <FaFacebook color="white" size={24} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://www.instagram.com/avi__potnuru"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="Instagram"
-            >
-              <FaInstagram color="white" size={24} />
-            </a>
-          </li>
-          <li>
-            <a
-              href="https://api.whatsapp.com/send/?phone=918919016096"
-              target="_blank"
-              rel="noopener noreferrer"
-              aria-label="WhatsApp"
-              className="text-green-500 text-3xl"
-            >
-              <FaWhatsapp size={24} />
-            </a>
-          </li>
+          {socialLinks.map(({ href, label, icon, className }) => (
+            <li key={label}>
+              <a
+                href={href}
+                target="_blank"
+                rel="noopener noreferrer"
+                aria-label={label}
+                className={className}
+              >
+                {icon}
+              </a>
+            </li>
+          ))}
         </ul>
       </footer>
     </>
